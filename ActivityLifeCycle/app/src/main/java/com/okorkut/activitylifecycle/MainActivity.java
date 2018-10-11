ad5e7b4ef88d881fe9b8be0d15ff853e2e27e68c
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-11 23:14:26 +0300

    Lifecycle için loglar yazıldı

--- ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.okorkut.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,35 +10,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("LifeCycle", "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i("LifeCycle", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("LifeCycle", "onCreate");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i("LifeCycle", "onPause");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.i("LifeCycle", "onRestart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i("LifeCycle", "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i("LifeCycle", "onDestroy");
     }
 }
