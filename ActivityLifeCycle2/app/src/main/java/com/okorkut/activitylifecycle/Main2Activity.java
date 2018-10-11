4e6542c4c8992cf602aa93dfdf16b952ea75b72c
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-12 00:58:51 +0300

    Activity örnek uygulama

--- ActivityLifeCycle2/app/src/main/java/com/okorkut/activitylifecycle/Main2Activity.java
+++ ActivityLifeCycle2/app/src/main/java/com/okorkut/activitylifecycle/Main2Activity.java
@@ -2,12 +2,35 @@ package com.okorkut.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView nameTV;
+    TextView lastnameTV;
+    TextView phoneNoTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        nameTV = findViewById(R.id.name);
+        lastnameTV = findViewById(R.id.lastname);
+        phoneNoTV = findViewById(R.id.phoneNo);
+
+        getData();
+    }
+
+    private void getData(){
+        Bundle intent = getIntent().getExtras();
+
+        String name = intent.getString("name");
+        String lastname =  intent.getString("lastname");
+        String phoneNo = intent.getString("phoneNo");
+
+        nameTV.setText(name);
+        lastnameTV.setText(lastname);
+        phoneNoTV.setText(phoneNo);
     }
 }
--- ActivityLifeCycle2/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle2/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createList();
     }
 
     private void createList(){
@@ -39,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         models.add(model5);
         models.add(model6);
         models.add(model7);
+
+        go();
     }
 
     public void go(){
