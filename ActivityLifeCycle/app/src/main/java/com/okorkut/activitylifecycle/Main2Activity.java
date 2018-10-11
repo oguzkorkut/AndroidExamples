575b2397e3c629e96225a8f3ff1be5d81985608c
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-12 00:06:03 +0300

    Activityler arası veri gönderme

--- ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/Main2Activity.java
+++ ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/Main2Activity.java
@@ -1,21 +1,51 @@
 package com.okorkut.activitylifecycle;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    String name;
+    String password;
+    String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        getPreviousActivityValues();
     }
 
     public void previousActivity(View view){
         Intent intent = new Intent(this,MainActivity.class);
 
         startActivity(intent);
+    }
+
+    private void getPreviousActivityValues(){
+        Bundle intent = getIntent().getExtras();
+
+        name = intent.getString("name");
+        password = intent.getString("password");
+        gender = intent.getString("gender");
+
+        StringBuilder builder = new StringBuilder("");
+
+        builder.append("Activity2->").append("name:").append(name).append(" ").append("Password:").append(password).append(" ").append("Gender:").append(gender);
+        Log.i(  "Form Data :" , builder.toString() );
+
+        TextView nameTV = findViewById(R.id.nameTV);
+        TextView passwordTV = findViewById(R.id.passwordTV);
+        TextView genderTV = findViewById(R.id.genderTV);
+
+        nameTV.setText("Name:" + name);
+        passwordTV.setText("Password:" + password);
+        genderTV.setText("Gender:" + gender);
+
     }
 }
--- ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/okorkut/activitylifecycle/MainActivity.java
@@ -1,12 +1,23 @@
 package com.okorkut.activitylifecycle;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nameET;
+    EditText passwordET;
+    Button sendBtn;
+    RadioGroup genderRG;
+
+    String name, password, gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void definition(){
+        nameET = findViewById(R.id.name);
+        passwordET = findViewById(R.id.password);
+        sendBtn = findViewById(R.id.nextBtn);
+        genderRG = findViewById(R.id.genderGroup);
+
+    }
+
+    private void getFormData(){
+        name = nameET.getText().toString();
+        password = passwordET.getText().toString();
+        gender = ((RadioButton)findViewById(genderRG.getCheckedRadioButtonId())).getText().toString();
+    }
+
     public void nextActivity(View view){
 
-        Intent intent = new Intent(this, Main2Activity.class);
+        getFormData();
+
+        StringBuilder builder = new StringBuilder("");
+
+        builder.append("Activity1->").append("name:").append(name).append(" ").append("Password:").append(password).append(" ").append("Gender:").append(gender);
+        Log.i(  "Form Data :" , builder.toString() );
+
+        //Intent intent = new Intent(this, Main2Activity.class);
+
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+
+        intent.putExtra( "name", name);
+        intent.putExtra("password",password);
+        intent.putExtra("gender", gender);
 
         startActivity(intent);
     }
@@ -29,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i("LifeCycle", "onStart");
+
+        definition();
     }
 
     @Override
