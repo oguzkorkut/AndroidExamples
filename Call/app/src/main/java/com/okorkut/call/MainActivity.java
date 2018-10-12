99a4972ef4df8202fc80f8a0595cea4a6e419acf
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-12 23:57:29 +0300

    Telefon arama uygulaması

--- Call/app/src/main/java/com/okorkut/call/MainActivity.java
+++ Call/app/src/main/java/com/okorkut/call/MainActivity.java
@@ -1,13 +1,34 @@
 package com.okorkut.call;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText phoneNoET;
+    Button callBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phoneNoET = findViewById(R.id.phoneNo);
+        callBtn = findViewById(R.id.callBtn);
+
+    }
+
+    public void call(View view){
+        String phoneNo = phoneNoET.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNo));
+
+        startActivity(intent);
     }
 }
