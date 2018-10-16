c828948581de9fa7fbee60e9def67163b88faf9e
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-17 00:39:13 +0300

    Büyük tansiyon ve küçük tansiyon değeleri alınarak grafik üzerinde gösterildi.

--- RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
+++ RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -34,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         ekleBtn = findViewById(R.id.ekle);
 
         barChart = findViewById(R.id.barChart);
+
+        goster();
+
+        //sil();
     }
 
     public void ekle(View view){
@@ -47,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
                 tansiyon.setKucukTansiyon(kucukTansiyonET.getText().toString());
             }
         });
+
+        listele();
+
+        goster();
     }
 
     public void listele(){
@@ -58,5 +71,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Log.i("Tansiyon Listesi", builder.toString());
+    }
+
+    public void goster(){
+        RealmResults<Tansiyon> tansiyons = realm.where(Tansiyon.class).findAll();
+
+        Float buyukTansiyon = 0f;
+        Float kucukTansiyon = 0f;
+
+        ArrayList<BarEntry> arrayList = new ArrayList<BarEntry>();
+
+        for (int i=0; i < tansiyons.size(); i++){
+            buyukTansiyon = buyukTansiyon + Float.parseFloat(tansiyons.get(i).getBuyukTansiyon());
+            kucukTansiyon = kucukTansiyon + Float.parseFloat(tansiyons.get(i).getKucukTansiyon());
+
+        }
+
+        arrayList.add(new BarEntry(buyukTansiyon, 0));
+        arrayList.add(new BarEntry(kucukTansiyon, 1));
+
+        BarDataSet barDataSet = new BarDataSet(arrayList, "Toplam Değer");
+
+        ArrayList<String> sutunIsmi = new ArrayList<String>();
+
+        sutunIsmi.add("Büyük Tansiyon");
+        sutunIsmi.add("Küçük Tansiyon");
+
+        BarData barData = new BarData(sutunIsmi, barDataSet);
+
+        barChart.setData(barData);
+        barChart.setDescription("Tansiyon Değerlerini Göster Grafik Arayüzü..");
+
+        barChart.invalidate();
+    }
+
+    public void sil(){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                //RealmQuery<Tansiyon> tansiyon = realm.where(Tansiyon.class).equalTo("buyukTansiyon", "13");
+                RealmResults<Tansiyon> tansiyons = realm.where(Tansiyon.class).findAll();
+                tansiyons.deleteFromRealm(1);
+            }
+        });
     }
 }
