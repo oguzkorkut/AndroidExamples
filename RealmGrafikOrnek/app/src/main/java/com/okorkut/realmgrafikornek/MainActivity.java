2f4df66ee31de6169b6ff547376c401476c4fe7d
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-16 23:52:04 +0300

    Barchart eklendi

--- RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
+++ RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
@@ -2,6 +2,11 @@ package com.okorkut.realmgrafikornek;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.mikephil.charting.charts.BarChart;
 
 import io.realm.Realm;
 
@@ -9,11 +14,34 @@ public class MainActivity extends AppCompatActivity {
 
     Realm realm;
 
+    EditText buyukTansiyonET, kucukTansiyonET;
+    Button ekleBtn;
+
+    BarChart barChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         realm = Realm.getDefaultInstance();
+
+        buyukTansiyonET = findViewById(R.id.buyukTansiyon);
+        kucukTansiyonET = findViewById(R.id.kucukTansiyon);
+
+        ekleBtn = findViewById(R.id.ekle);
+
+        barChart = findViewById(R.id.barChart);
+    }
+
+    public void ekle(View view){
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                
+            }
+        });
+
     }
 }
--- RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/Tansiyon.java
+++ RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/Tansiyon.java
@@ -3,4 +3,31 @@ package com.okorkut.realmgrafikornek;
 import io.realm.RealmObject;
 
 public class Tansiyon extends RealmObject {
+
+    private String buyukTansiyon;
+    private String kucukTansiyon;
+
+    public String getBuyukTansiyon() {
+        return buyukTansiyon;
+    }
+
+    public void setBuyukTansiyon(String buyukTansiyon) {
+        this.buyukTansiyon = buyukTansiyon;
+    }
+
+    public String getKucukTansiyon() {
+        return kucukTansiyon;
+    }
+
+    public void setKucukTansiyon(String kucukTansiyon) {
+        this.kucukTansiyon = kucukTansiyon;
+    }
+
+    @Override
+    public String toString() {
+        return "Tansiyon{" +
+                "buyukTansiyon='" + buyukTansiyon + '\'' +
+                ", kucukTansiyon='" + kucukTansiyon + '\'' +
+                '}';
+    }
 }
