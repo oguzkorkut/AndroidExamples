a31fc5d321a0f8b4eda0b3cc549fae7f2a0883ee
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-16 23:43:18 +0300

    MainActivity clasında realm oluşturuldu

--- RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
+++ RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/MainActivity.java
@@ -1,13 +1,19 @@
 package com.okorkut.realmgrafikornek;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
+
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        realm = Realm.getDefaultInstance();
     }
 }
