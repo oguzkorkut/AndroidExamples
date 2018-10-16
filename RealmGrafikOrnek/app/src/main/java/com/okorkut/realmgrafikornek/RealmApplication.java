07d429981289e8097ce94df44aa99845d135fc75
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-16 23:41:12 +0300

    Realm tanımlandı

--- RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/RealmApplication.java
+++ RealmGrafikOrnek/app/src/main/java/com/okorkut/realmgrafikornek/RealmApplication.java
@@ -2,5 +2,17 @@ package com.okorkut.realmgrafikornek;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class RealmApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("exampleRealm").build();
+        Realm.setDefaultConfiguration(config);
+    }
 }
