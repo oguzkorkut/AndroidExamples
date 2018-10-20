7e52075bf8bea6ba3a2922a7eafd125d25ff1081
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-20 23:36:05 +0300

    Quiz sukor ekrano yapıldı

--- FlagQuiz/app/src/main/java/com/okorkut/flagquiz/Score.java
+++ FlagQuiz/app/src/main/java/com/okorkut/flagquiz/Score.java
@@ -1,13 +1,31 @@
 package com.okorkut.flagquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.okorkut.flagquiz.common.CustomAdapter;
+import com.okorkut.flagquiz.dbhelper.DBHelper;
+import com.okorkut.flagquiz.model.Ranking;
+
+import java.util.List;
 
 public class Score extends AppCompatActivity {
+
+    ListView lstView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        lstView = (ListView)findViewById(R.id.lstRanking);
+        DBHelper db = new DBHelper(this);
+        List<Ranking> lstRanking = db.getRanking();
+        if(lstRanking.size() > 0)
+        {
+            CustomAdapter adapter = new CustomAdapter(this,lstRanking);
+            lstView.setAdapter(adapter);
+        }
     }
 }
