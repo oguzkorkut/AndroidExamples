eef92f8bd1c466ba4fea22fb142b6a971a930d40
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-23 20:07:41 +0300

    Magic UI Button

--- ButtonUI/app/src/main/java/com/okorkut/buttonui/MainActivity.java
+++ ButtonUI/app/src/main/java/com/okorkut/buttonui/MainActivity.java
@@ -2,12 +2,51 @@ package com.okorkut.buttonui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import br.com.bloder.magic.view.MagicButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MagicButton btnFacebook, btnYoutube, btnTwitter, btnGithub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFacebook = findViewById(R.id.facebook_magic_button);
+        btnYoutube = findViewById(R.id.youtube_magic_button);
+        btnTwitter = findViewById(R.id.twitter_magic_button);
+        btnGithub = findViewById(R.id.github_magic_button);
+
+        btnFacebook.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Facebook", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnYoutube.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Youtube", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnTwitter.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Twitter", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGithub.setMagicButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Github", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
