232e31f3d4ad603296c85fc7796364052f395505
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-25 22:01:11 +0300

    Giriş ve kayıt işlemleri yapıldı.

--- OnlineQuizApp/app/src/main/java/com/okorkut/onlinequizapp/MainActivity.java
+++ OnlineQuizApp/app/src/main/java/com/okorkut/onlinequizapp/MainActivity.java
@@ -1,17 +1,26 @@
 package com.okorkut.onlinequizapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.okorkut.onlinequizapp.model.User;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    MaterialEditText edtNewUser,edtNewPAssword,edtNewEmail;
+    MaterialEditText edtNewUser,edtNewPassword,edtNewEmail;
     MaterialEditText edtUser,edtPassword;
 
     Button btnSignUp, btnSignIn;
@@ -37,8 +46,95 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                showSignUpDialog();
             }
         });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(edtUser.getText().toString(), edtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void signIn(final String user, final String password) {
+
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(user).exists()){
+                    if (!user.isEmpty()){
+                        User login = dataSnapshot.child(user).getValue(User.class);
+                        if(login.getPassword().equalsIgnoreCase(password)){
+                            Toast.makeText(MainActivity.this,"Login ok!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this,"Wrong password", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this,"User is not exists!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void showSignUpDialog(){
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        View sign_up_layout = inflater.inflate(R.layout.sign_up_layout, null);
+
+
+        edtNewUser = findViewById(R.id.edtNewUserName);
+        edtNewPassword = findViewById(R.id.edtNewPassword);
+        edtNewEmail = findViewById(R.id.edtNewEmail);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final User user = new User(edtNewUser.getText().toString(), edtNewPassword.getText().toString(),edtNewEmail.getText().toString());
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getUserName()).exists()){
+                            Toast.makeText(MainActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            users.child(user.getUserName()).setValue(user);
+                            Toast.makeText(MainActivity.this, "User registration success!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 }
