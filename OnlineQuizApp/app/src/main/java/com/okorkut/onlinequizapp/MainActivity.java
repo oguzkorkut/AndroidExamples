a73eb020e78ddb153edff67e0f07ba4d49e1dd0e
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-25 21:30:32 +0300

    Giriş ekran tasarımı yapıldı.

--- OnlineQuizApp/app/src/main/java/com/okorkut/onlinequizapp/MainActivity.java
+++ OnlineQuizApp/app/src/main/java/com/okorkut/onlinequizapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.okorkut.onlinequizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUser,edtNewPAssword,edtNewEmail;
+    MaterialEditText edtUser,edtPassword;
+
+    Button btnSignUp, btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        edtUser = findViewById(R.id.edtUser);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
