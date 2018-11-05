cad1be0b5fc25f12e2a53414c980780c0344623c
Oğuz Korkut <oguzkrkt@gmail.com>
2018-11-05 23:54:27 +0300

    Navigation menu ayarları tamamlandı

--- SocialNetworkApp/app/src/main/java/com/okorkut/socialnetworkapp/MainActivity.java
+++ SocialNetworkApp/app/src/main/java/com/okorkut/socialnetworkapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawable_layout);
         navigationView = findViewById(R.id.navigation_view);
+
+        View view = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        
 
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
