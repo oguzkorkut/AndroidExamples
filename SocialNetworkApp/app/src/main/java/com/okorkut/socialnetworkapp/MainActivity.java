90cfcd158771ea09f6291536d00e0c776f74fae7
Oğuz Korkut <oguzkrkt@gmail.com>
2018-11-06 18:56:24 +0300

    Toolbar çalışır duruma getirildi

--- SocialNetworkApp/app/src/main/java/com/okorkut/socialnetworkapp/MainActivity.java
+++ SocialNetworkApp/app/src/main/java/com/okorkut/socialnetworkapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.okorkut.socialnetworkapp;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView postList;
     private Toolbar mToolbar;
 
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,17 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         mToolbar.setTitle("Home");
-        //setSupportActionBar(mToolbar);
-        //getSupportActionBar().setTitle("Home");
 
+        this.setSupportActionBar(mToolbar);
+
+        this.getSupportActionBar().setTitle("Home");
+
+        //this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         drawerLayout = findViewById(R.id.drawable_layout);
         navigationView = findViewById(R.id.navigation_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         View view = navigationView.inflateHeaderView(R.layout.navigation_header);
-
-
-
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -46,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private  void userMenuSelector(MenuItem item) {
