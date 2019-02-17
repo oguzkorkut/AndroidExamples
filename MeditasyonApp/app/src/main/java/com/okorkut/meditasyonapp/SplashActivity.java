1a9900b428d982df7285463119b9451ebd932cdb
Oğuz Korkut <oguzkrkt@gmail.com>
2019-02-17 23:54:13 +0300

    İnternet bağlantısı kontrolü kondu

--- MeditasyonApp/app/src/main/java/com/okorkut/meditasyonapp/SplashActivity.java
+++ MeditasyonApp/app/src/main/java/com/okorkut/meditasyonapp/SplashActivity.java
@@ -1,13 +1,58 @@
 package com.okorkut.meditasyonapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    public boolean internetVarMi(final Context context){
+
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if (internetVarMi(this)){
+            Thread splashThread = new Thread(){
+                @Override
+                public void run(){
+                    synchronized (this){
+                        try {
+                            wait(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                        }
+                    }
+                }
+            };
+            splashThread.start();
+        } else {
+            AlertDialog alert = new AlertDialog.Builder(this).create();
+
+            alert.setTitle("Bağlantı Hatası");
+            alert.setMessage("Lütfen internet bağlantınızı kontrol ediniz..");
+            alert.setButton(DialogInterface.BUTTON_NEUTRAL, "TAMAM", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //int pid = android.Pro
+                    dialog.dismiss();
+                }
+            });
+            alert.show();
+        }
+
     }
 }
